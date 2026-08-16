5b3af0cfa837f8d25c3942f794f2a76550310eb0
Sanjana Pisal <sanjanapisal02@gmail.com>
2026-08-16 11:42:11 +0530

    updated value

--- Basics/Basic_Programs/BuiltinMethods.py
+++ Basics/Basic_Programs/BuiltinMethods.py
@@ -2,7 +2,7 @@ x = 3.14
 y = -4
 z = 5
 
-# result = round(x)  output = 3
+result = round(x)  # output = 3
 # result = abs(y)  output = 4 (when -4)
 # result = pow (4,3)    output = 64
 # result = max(x, y, z)     output= 5
