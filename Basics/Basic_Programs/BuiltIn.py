455c8722e63b8710f69061c1a074dfd54d1d95a9
Sanjana Pisal <sanjanapisal02@gmail.com>
2026-08-16 11:40:24 +0530

    updated value

--- Basics/Basic_Programs/BuiltIn.py
+++ Basics/Basic_Programs/BuiltIn.py
@@ -1,15 +1,16 @@
 import math
 
-print(math.py)
+# print(math.py)
 #output = 3.14159...
 
 print(math.e)
 #output = 2.178...
 
-# result = math.sqrt(x)
+x = 3
+result = math.sqrt(x)
 #output = 3.0
 
-# result = math.ceil(x) if x ix 9.2
+# result = math.ceil(x) if x is 9.2
 #output = 10
 
 # result = math.floor(x) if x is 9.8
